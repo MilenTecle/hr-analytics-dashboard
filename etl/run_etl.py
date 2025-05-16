5d7fd661bee687d92a5cf1dc512290d64b44fef4
MilenTecle <m_tecle@hotmail.com>
2025-05-16 20:19:38 +0200

    Fix module import error in run_etl.py for GitHub Actions

--- etl/run_etl.py
+++ etl/run_etl.py
@@ -4,7 +4,6 @@ from urllib.parse import urlparse
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from config import DATABASE_URL
 
 # Path to the cleaned HR data CSV file
